0b5b0a5336649658b3acacb10a0a8f1d02caa9b6
Vishal Yathish <vishal.yathish@gmail.com>
2024-12-22 20:39:09 -0800

    reorder helper functions

--- hw1.ml
+++ hw1.ml
@@ -16,11 +16,9 @@ match a with
 | [] -> []
 | h::t -> set_union h (set_all_union t);;
 
-(* 
-5. Russell's Paradox – this is impossible in OCaml. 
+(** 5. Russell's Paradox – this is impossible in OCaml. 
 Expressions of type 'a list cannot in turn be composed of their own type 
-(recursively, with no base type 'a). 
-The resulting error will be produced: 
+(recursively, with no base type 'a). The resulting error will be produced: 
 "This expression has type 'a but an expression was expected of type 'a list. 
 The type variable 'a occurs inside 'a list" 
 *)
@@ -32,26 +30,13 @@ match x with
 | x -> computed_fixed_point eq f (f x);;
 
 (** 7. Computed Periodic Point *)
-let rec computed_periodic_point eq f p x = 
-match x with 
-| x when eq x (p_periodic f p x) -> x 
-| x -> computed_periodic_point eq f p (f x);;
-
 let rec p_periodic f p x = 
 match p with 
 | 0 -> x 
 | p -> p_periodic f (p-1) (f x);;
 
-(** 8. Longest Sequence *)
-(* longest sequence [x, s x, s (s x), s (s (s x)), ...] 
-* such that p e is true for every element *)
-
-let whileseq s p x = helper s p x [];; 
-
-let rec helper s p x seq = 
+let rec computed_periodic_point eq f p x = 
 match x with 
-| x when p x -> helper s p (s x) seq@x 
-| x -> seq;;
+| x when eq x (p_periodic f p x) -> x 
+| x -> computed_periodic_point eq f p (f x);;
 
-(** 9. Filter Blind Alleys *)
-let filter_blind_alleys g = 
